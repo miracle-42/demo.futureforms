286c441626530497364265868d33523b18684a24
Peter Gram <peter.m.gram@gmail.com>
2023-06-29 19:47:28 +0200

    Add 0-empty that neds to be slimes down to remove the login code. Added install yml for for 1-login

---
+++ src/0-empty/webpack.config.cjs
@@ -0,0 +1,42 @@
+module.exports =
+{
+  devtool: "eval-source-map",
+  entry: './src/index.ts',
+  output:
+  {
+    path: __dirname+'/../../build/0-empty/app',
+    filename: 'app.js',
+    library: "forms42core"
+  },
+  module:
+  {
+    rules:
+    [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use:
+        {
+          loader: 'ts-loader'
+        }
+      }
+      ,
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  }
+  ,
+  resolve:
+  {
+    extensions: ['.ts','.js']
+  }
+  ,
+  performance:
+  {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000
+  }
+}
